7bd5c32b747bbe978f3198bd5accf5a9a6ca05a8
Augusto Andrade <webgutodev@gmail.com>
2021-11-10 16:24:34 -0300

    Enumeracao e Composicao

---
+++ secao9-enumeracoes-composicao/aula119_121-ex-enumeracoes/Enumeracao/Enumeracao/Entities/Worker.cs
@@ -0,0 +1,25 @@
+﻿using Enumeracao.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enumeracao.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+
+        public Worker() 
+        { 
+        }
+        public Worker(string name, WorkerLevel level, double baseSalary)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+        }
+    }
+}
